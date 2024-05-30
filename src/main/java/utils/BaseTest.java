6db7ac820fd0615c1467faec73d75898171f4bd0
Dragos Tanta <dragos.tanta@gmail.com>
2024-05-30 11:24:42 +0300

    curs24 testng.xml

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -5,8 +5,11 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import pages.LoginPage;
 import pages.MenuPage;
@@ -17,6 +20,7 @@ public class BaseTest {
 	public MenuPage menu;
 	public LoginPage login;
 	
+	//@Parameters({"url"})
 	@BeforeClass
 	public void setup() {
 		
@@ -24,6 +28,7 @@ public class BaseTest {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://keybooks.ro/");
+		//driver.get(appUrl);
 		
 	}
 	
